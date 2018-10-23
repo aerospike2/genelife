51a556dc72135b3bfa280452ae1ea3e31cc08ffb
John S. McCaskill <john.mccaskill@rub.de>
2018-10-23 22:08:54 +0200

    Error message if offsets for out config have positive z (into the future not allowed).

--- fastgenegol/genelifec/subgenelife.c
+++ fastgenegol/genelifec/subgenelife.c
@@ -674,10 +674,13 @@ void initialize_planes(int offs[],  int No) {
     int toff, tmn = No;
     for(i=0; i<Noff; i++){
 	    toff = offsets[i][2];
-	    // if(toff>tmx) tmx = toff; // it does not make sense to have +ve values (this would look into future) so tmx = 0
+	    if(toff>tmx) tmx = toff; // it does not make sense to have +ve values (this would look into future) so tmx = 0
 	    if(toff<tmn) tmn = toff;
     }
-    //    fprintf(stderr,"----------------- tmx = %d, tmn = %d",tmx,tmn);
+    if(tmx>0)   {
+        fprintf(stderr,"----------------- tmx = %d, tmn = %d",tmx,tmn);
+        exit(1);
+    }
     tall = tmx-tmn;
     // numPlane = 2 + tall;    // numPlane >= 2
     if(tall>4) numPlane = 8;
