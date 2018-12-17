427bcb1dd04291b7b3cc67b01b3d43c95610ce62
John S. McCaskill <john.mccaskill@rub.de>
2018-12-17 10:23:52 +0100

    Just updating reference pdfs

---
+++ Golly-3.2-Mac/golly-3.2-src/gui-ios/hashtest/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hashtest
+//
+//  Created by John McCaskill on 16/12/2018.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
