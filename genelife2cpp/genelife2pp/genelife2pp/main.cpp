759c0d060af138b599826fa3a28380e37040ed84
John S. McCaskill <john.mccaskill@rub.de>
2019-03-18 22:34:04 +0100

    python 3 version and various updates

---
+++ genelife2cpp/genelife2pp/genelife2pp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  genelife2pp
+//
+//  Created by John McCaskill on 16/03/2019.
+//  Copyright © 2019 European Centre for Living Technology. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
